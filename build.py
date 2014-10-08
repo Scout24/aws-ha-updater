3fae0baee51793151ad7f9140d2e75ec15bc8bbe
mriehl <max@riehl.io>
2014-10-08 09:28:59 +0200

    introduce coverage metrics

--- build.py
+++ build.py
@@ -1,6 +1,7 @@
 from pybuilder.core import use_plugin, init
 
 use_plugin("python.core")
+use_plugin("python.coverage")
 use_plugin("python.unittest")
 use_plugin("python.install_dependencies")
 use_plugin("python.flake8")
@@ -22,6 +23,7 @@ def set_properties(project):
     project.depends_on('boto')
 
     project.set_property('flake8_verbose_output', True)
+    project.set_property('coverage_break_build', False)
     project.set_property('flake8_include_test_sources', True)
     project.set_property('flake8_include_scripts', True)
     project.set_property('flake8_ignore', 'E501')
