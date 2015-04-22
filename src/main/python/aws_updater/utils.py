6c89f36752683366f16690f652b42e9de6d8003e
Marco Hoyer <marco@hoyer.pm>
2015-04-22 18:50:51 +0200

    add a simple logger configuration method

--- src/main/python/aws_updater/utils.py
+++ src/main/python/aws_updater/utils.py
@@ -1,5 +1,6 @@
 from functools import wraps
 from time import time
+import logging
 
 
 def timed(function):
@@ -13,3 +14,10 @@ def timed(function):
         return result
 
     return wrapper
+
+
+def get_logger():
+    logging.basicConfig(format='%(asctime)s %(levelname)s %(module)s: %(message)s',
+                        datefmt='%d.%m.%Y %H:%M:%S',
+                        level=logging.INFO)
+    return logging.getLogger(__name__)
